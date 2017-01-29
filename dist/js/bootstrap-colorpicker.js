004ca000384d6c27b71280d8bbc7592cf674ced7
n1crack <yusuf@ozdemir.be>
2017-01-29 20:54:53 +0300

    fix on plugin wrapper in order to work with webpack

--- dist/js/bootstrap-colorpicker.js
+++ dist/js/bootstrap-colorpicker.js
@@ -8,23 +8,21 @@
  *
  */
 
-(function(factory) {
-  var global = (typeof window === 'undefined') ? this : window;
-
-  if (typeof exports === 'object') {
-    module.exports = factory(global.jQuery, global);
-  } else if (typeof define === 'function' && define.amd) {
-    define(['jquery'], function(jq) {
-      return factory(jq, global);
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    define(["jquery"], function(a0) {
+      return (factory(a0));
     });
-  } else if (global.jQuery && !global.jQuery.fn.colorpicker) {
-    factory(global.jQuery, global);
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(require("jquery"));
+  } else {
+    factory(jQuery);
   }
-}(function(jQuery, window) {
-  'use strict';
-
-  var $ = jQuery;
-
+}(this, function($) {
   /**
    * Color manipulation helper class
    *

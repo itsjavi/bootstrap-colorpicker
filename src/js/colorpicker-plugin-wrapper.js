ef1b6efb23bc227e17dafb92a70c8044860acb40
n1crack <yusuf@ozdemir.be>
2017-01-30 19:38:48 +0300

    use strict, prevent duplicate loading.

--- src/js/colorpicker-plugin-wrapper.js
+++ src/js/colorpicker-plugin-wrapper.js
@@ -11,18 +11,19 @@
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module unless amdModuleId is set
-    define(["jquery"], function(a0) {
-      return (factory(a0));
+    define(["jquery"], function(jq) {
+      return (factory(jq));
     });
   } else if (typeof exports === 'object') {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
     module.exports = factory(require("jquery"));
-  } else {
+  } else if (jQuery && !jQuery.fn.colorpicker) {
     factory(jQuery);
   }
 }(this, function($) {
+  'use strict';
   //@colorpicker-color
   //@colorpicker-defaults
   //@colorpicker-component

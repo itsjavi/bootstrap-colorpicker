fb682f34108fd2754e2c586cee25ded135fa5c2d
Javi Aguilar <git@itsjavi.com>
2018-07-25 00:17:48 +0200

    fix options docs and rearrange properties

--- src/js/options.js
+++ src/js/options.js
@@ -17,11 +17,12 @@ let sliderSize = (sassVars.bar_size_short * sassVars.columns) + (sassVars.base_m
  */
 export default {
   /**
-   * If true, loads the Debugger extension automatically into the current instance
-   * @type {boolean}
-   * @default false
+   * Custom class to be added to the `.colorpicker-element` element
+   *
+   * @type {String|null}
+   * @default null
    */
-  debug: false,
+  customClass: null,
   /**
    * Sets a initial color, ignoring the one from the element/input value or the data-color attribute.
    *
@@ -30,13 +31,21 @@ export default {
    */
   color: false,
   /**
+   * Fallback color to use when the given color is invalid.
+   * If false, the latest valid color will be used as a fallback.
+   *
+   * @type {String|Color|boolean}
+   * @default false
+   */
+  fallbackColor: false,
+  /**
    * Forces an specific color format. If 'auto', it will be automatically detected the first time only,
    * but if null it will be always recalculated.
    *
    * Note that the ending 'a' of the format meaning "alpha" has currently no effect, meaning that rgb is the same as
    * rgba excepting if the alpha channel is disabled (see useAlpha).
    *
-   * @type {('rgb'|'rgba'|'prgb'|'prgba'|'hex'|'hex3'|'hex6'|'hex8'|'hsl'|'hsla'|'hsv'|'hsva'|'name'|boolean)}
+   * @type {('rgb'|'rgba'|'prgb'|'prgba'|'hex'|'hex3'|'hex6'|'hex8'|'hsl'|'hsla'|'hsv'|'hsva'|'name'|'auto'|null)}
    * @default null
    */
   format: null,
@@ -50,12 +59,27 @@ export default {
    */
   horizontal: false,
   /**
-   * Forces to show the colorpicker as an inline element
+   * Forces to show the colorpicker as an inline element.
+   *
+   * Note that if there is no container specified, the inline element
+   * will be added to the body, so you may want to set the container option.
    *
    * @type {boolean}
    * @default false
    */
   inline: false,
+  /**
+   * Container where the colorpicker is appended to in the DOM.
+   *
+   * If is a string (CSS selector), the colorpicker will be placed inside this container.
+   * If true, the `.colorpicker-element` element itself will be used as the container.
+   * If false, the document body is used as the container, unless it is a popover (in this case it is appended to the
+   * popover body instead).
+   *
+   * @type {String|boolean}
+   * @default false
+   */
+  container: false, // container selector
   /**
    * Bootstrap Popover options. If false, the built-in popover will be used.
    * The trigger, content and html options are always ignored.
@@ -69,22 +93,18 @@ export default {
     fallbackPlacement: 'flip'
   },
   /**
+   * If true, loads the 'debugger' extension automatically, which logs the events in the console
+   * @type {boolean}
+   * @default false
+   */
+  debug: false,
+  /**
    * Child input CSS selector
    *
    * @type {String}
    * @default 'input'
    */
   input: 'input',
-  /**
-   * Colorpicker container CSS selector.
-   * If is a string (CSS selector), the colorpicker will be placed inside this container.
-   * If true, the `.colorpicker-element` element itself will be used as the container.
-   * If false or null, the document body is used as the container.
-   *
-   * @type {String|boolean}
-   * @default false
-   */
-  container: false, // container selector
   /**
    * Child color component CSS selector.
    * If it exists, the child <i> element background will be changed on color change.
@@ -93,14 +113,6 @@ export default {
    * @default '.colorpicker-input-addon'
    */
   component: '.colorpicker-input-addon',
-  /**
-   * Fallback color to use when the given color is invalid.
-   * If false, the latest valid color will be used as a fallback.
-   *
-   * @type {String|Color|boolean}
-   * @default false
-   */
-  fallbackColor: false,
   /**
    * If true, the input content will be replaced always with a valid color,
    * if false, the invalid color will be left in the input,
@@ -116,7 +128,7 @@ export default {
    * This only affects the input values in hexadecimal format.
    *
    * @type {boolean}
-   * @default false
+   * @default true
    */
   useHashPrefix: true,
   /**
@@ -142,9 +154,62 @@ export default {
    * @see https://caniuse.com/#feat=css-rrggbbaa
    *
    * @type {boolean}
-   * @default true
+   * @default false
    */
   enableHexAlpha: false,
+  /**
+   * Colorpicker widget template
+   * @type {String}
+   * @example
+   * <!-- This is the default template: -->
+   * <div class="colorpicker">
+   *   <div class="colorpicker-saturation"><i class="colorpicker-guide"></i></div>
+   *   <div class="colorpicker-hue"><i class="colorpicker-guide"></i></div>
+   *   <div class="colorpicker-alpha">
+   *     <div class="colorpicker-alpha-color"></div>
+   *     <i class="colorpicker-guide"></i>
+   *   </div>
+   * </div>
+   */
+  template: `<div class="colorpicker">
+      <div class="colorpicker-saturation"><i class="colorpicker-guide"></i></div>
+      <div class="colorpicker-hue"><i class="colorpicker-guide"></i></div>
+      <div class="colorpicker-alpha">
+        <div class="colorpicker-alpha-color"></div>
+        <i class="colorpicker-guide"></i>
+      </div>
+    </div>`,
+  /**
+   *
+   * Associative object with the extension class name and its config.
+   * Colorpicker comes with many bundled extensions: debugger, palette, preview and swatches (a superset of palette).
+   *
+   * @type {Object[]}
+   * @example
+   *   extensions: [
+   *     {
+   *       name: 'swatches'
+   *       options: {
+   *         colors: {
+   *           'primary': '#337ab7',
+   *           'success': '#5cb85c',
+   *           'info': '#5bc0de',
+   *           'warning': '#f0ad4e',
+   *           'danger': '#d9534f'
+   *         },
+   *         namesAsValues: true
+   *       }
+   *     }
+   *   ]
+   */
+  extensions: [
+    {
+      name: 'preview',
+      options: {
+        showText: false
+      }
+    }
+  ],
   /**
    * Vertical sliders configuration
    * @type {Object}
@@ -200,60 +265,5 @@ export default {
       callLeft: 'setAlphaRatio',
       callTop: false
     }
-  },
-  /**
-   * Custom class to be added to the colorpicker element
-   *
-   * @type {String}
-   */
-  customClass: null,
-  /**
-   * Colorpicker widget template
-   * @type {String}
-   * @example
-   * <!-- This is the default template: -->
-   * <div class="colorpicker">
-   *   <div class="colorpicker-saturation"><i class="colorpicker-guide"><i></i></i></div>
-   *   <div class="colorpicker-hue"><i class="colorpicker-guide"></i></div>
-   *   <div class="colorpicker-alpha"><i class="colorpicker-guide"></i></div>
-   *   <div class="colorpicker-color"><div /></div>
-   * </div>
-   */
-  template: `<div class="colorpicker">
-      <div class="colorpicker-saturation"><i class="colorpicker-guide"></i></div>
-      <div class="colorpicker-hue"><i class="colorpicker-guide"></i></div>
-      <div class="colorpicker-alpha">
-          <div class="colorpicker-alpha-color"></div>
-          <i class="colorpicker-guide"></i>
-      </div>
-    </div>`,
-  /**
-   *
-   * Associative object with the extension class name and its config.
-   * Colorpicker comes with many bundled extensions: debugger, palette, preview and swatches (a superset of Palette).
-   *
-   * @type {Object}
-   * @example
-   *   extensions: [
-   *     {
-   *       name: 'swatches'
-   *       colors: {
-   *         'primary': '#337ab7',
-   *         'success': '#5cb85c',
-   *         'info': '#5bc0de',
-   *         'warning': '#f0ad4e',
-   *         'danger': '#d9534f'
-   *       },
-   *       namesAsValues: true
-   *     }
-   *   ]
-   */
-  extensions: [
-    {
-      name: 'preview',
-      options: {
-        showText: false
-      }
-    }
-  ]
+  }
 };

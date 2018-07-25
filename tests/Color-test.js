05ca9f2946bc976e5b5c8746a4f2bd029e5ea244
Javi Aguilar <git@itsjavi.com>
2018-07-25 19:19:04 +0200

    tests

--- tests/Color-test.js
+++ tests/Color-test.js
@@ -1,57 +1,25 @@
 //'use strict';
 
-import TColor from '../src/js/Color';
+import TColor from '../src/js/ColorItem';
 import test from 'ava';
 
-let tests = {
-  "hex6": {
-    "#5367ce": "#5367ce",
-    "#5367ce55": "#5367ce",
-    "invalid": "#000000",
-    "rgb(83, 103, 206)": "#5367ce"
-  },
-  "hex8": {
-    "#5367ce": "#5367ceff",
-    "#5367ce55": "#5367ce55",
-    "invalid": "#000000ff",
-    "rgb(83, 103, 206)": "#5367ceff"
-  },
-  "rgb": {
-    "#5367ce": "rgb(83, 103, 206)",
-    "#5367ce55": "rgba(83, 103, 206, 0.33)",
-    "invalid": "rgb(0, 0, 0)",
-    "rgb(83, 103, 206)": "rgb(83, 103, 206)"
-  }
-};
+let dataset = [
+  // hex
+  ["#5367ce", "#5367CE", 'hex'],
+  ["#5367ce55", "#5367CE", 'hex'],
+  ["invalid", "#000000", 'hex'],
+  ["rgb(83, 103, 206)", "#5367CE", 'hex'],
+  // rgb
+  ["#5367ce", "rgb(83, 103, 206)", 'rgb'],
+  ["#5367ce55", "rgba(83, 103, 206, 0.33)", 'rgb'],
+  ["invalid", "rgb(0, 0, 0)", 'rgb'],
+  ["rgb(83, 103, 206)", "rgb(83, 103, 206)", 'rgb']
+];
 
-tests.rgba = tests.rgb;
+dataset.forEach(function (data, i) {
+  let value = data[0], expectedValue = data[1], format = data[2];
 
-let runColorTest = function (title, data, fn, fnArgs = [], ctorArgs = []) {
-  test(title, t => {
-    for (let colorInput in data) {
-      if (!data.hasOwnProperty(colorInput)) continue;
-      let expectedColorOutput = data[colorInput];
-
-      // Lower case input
-      let color = new TColor(colorInput, ...ctorArgs);
-      t.is(color[fn].call(color, ...fnArgs), expectedColorOutput);
-
-      // Upper case input
-      color = new TColor(colorInput.toUpperCase(), ...ctorArgs);
-      t.is(color[fn].call(color, ...fnArgs), expectedColorOutput);
-
-      // Input without hash
-      color = new TColor(colorInput.replace(/^#/g, ''), ...ctorArgs);
-      t.is(color[fn].call(color, ...fnArgs), expectedColorOutput);
-
-      // Input without hash, uppercase
-      color = new TColor(colorInput.replace(/^#/g, '').toUpperCase(), ...ctorArgs);
-      t.is(color[fn].call(color, ...fnArgs), expectedColorOutput);
-    }
+  test(`ColorItem.string #${i} expects ${expectedValue}`, t => {
+    t.is((new TColor(value)).string(format), expectedValue);
   });
-};
-
-for (let format in tests) {
-  if (!tests.hasOwnProperty(format)) continue;
-  runColorTest(`Color.toString returns a ${format} color`, tests[format], 'toString', [format], []);
-}
+});

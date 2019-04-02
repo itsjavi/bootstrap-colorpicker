89a3a20784f762cfee862128e6dde3e41bbc0544
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2019-04-02 23:41:22 +0200

    fix ColorItem.replace and fix builds

--- tests/Color-test.js
+++ tests/Color-test.js
@@ -1,19 +1,19 @@
-//'use strict';
+// 'use strict';
 
 import ColorItem from '../src/js/ColorItem';
 import test from 'ava';
 
 let dataset = [
   // hex
-  ["#5367ce", "#5367CE", 'hex'],
-  ["#5367ce55", "#5367CE", 'hex'],
-  ["invalid", "#000000", 'hex'],
-  ["rgb(83, 103, 206)", "#5367CE", 'hex'],
+  ['#5367ce', '#5367CE', 'hex'],
+  ['#5367ce55', '#5367CE', 'hex'],
+  ['invalid', '#000000', 'hex'],
+  ['rgb(83, 103, 206)', '#5367CE', 'hex'],
   // rgb
-  ["#5367ce", "rgb(83, 103, 206)", 'rgb'],
-  ["#5367ce55", "rgba(83, 103, 206, 0.33)", 'rgb'],
-  ["invalid", "rgb(0, 0, 0)", 'rgb'],
-  ["rgb(83, 103, 206)", "rgb(83, 103, 206)", 'rgb']
+  ['#5367ce', 'rgb(83, 103, 206)', 'rgb'],
+  ['#5367ce55', 'rgba(83, 103, 206, 0.33)', 'rgb'],
+  ['invalid', 'rgb(0, 0, 0)', 'rgb'],
+  ['rgb(83, 103, 206)', 'rgb(83, 103, 206)', 'rgb']
 ];
 
 dataset.forEach(function (data, i) {
